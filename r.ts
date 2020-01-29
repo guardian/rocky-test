685797cda9a8b9e2dea40fd9e8e0e5c59fc659fd
Alex Ware <alex.ware@guardian.co.uk>
2020-01-29 11:19:13 +0000

    TEST!

---
+++ r.ts
@@ -0,0 +1,25 @@
+import { Rocky } from '@guardian/rocky'
+
+const rocky = new Rocky({
+  name: "Rocky",
+  url: "https://github.com/guardian/Rocky",
+  parameters: {
+    "parameterName": {
+      description: "This is where some data goes"
+    }
+  },
+  bucket: "rocky-test-dist",
+  stacks: ['playground']
+})
+
+const deployment = rocky.deployment({
+  name: "deployment",
+  path: "./dist"
+})
+
+const lambda = rocky.lambda({
+  name: "lambda",
+  handler: "index.handler",
+  deployment
+})
+
